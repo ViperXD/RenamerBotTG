90eb8642824c2da1fbc91e64196f22ac906d3b04
Aid3n_Pearce <61540835+Aid-3n@users.noreply.github.com>
2020-05-17 21:28:35 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -30,7 +30,7 @@ No Help IS Provided🤷‍♂️🤷‍♂️🤣"""
    
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.."
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
-    FREE_USER_LIMIT_Q_SZE = """Cannot Process.
+    FREE_USER_LIMIT_Q_SZE = "No Limits"
 Free users only 1 request per 30 minutes.
 /upgrade or Try 1800 seconds later."""
     IFLONG_FILE_NAME = """File Name limit allowed by Telegram is {alimit} characters.
