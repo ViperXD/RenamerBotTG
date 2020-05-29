e8064c72715cd4a66b42ce9a09b7c66d005c3630
Aid3n_Pearce <61540835+Aid-3n@users.noreply.github.com>
2020-05-29 23:45:08 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,26 +1,23 @@
 class Translation(object):
-    START_TEXT = """ Simple Telegram Rename BOT Powered By GPLV3
+    START_TEXT = """ Simple Telegram Renaming BOT Made By Human
 
-<b>Please send me any Telegram file and reply /rename New Name.extn</b>
-Searching For Help? 
-NO,Mate 
-No Help IS Provided🤷‍♂️🤷‍♂️🤣"""
+<b>Please send me any Telegram file and reply /rename New Name.extn</b>"""
 
-    RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
+    RENAME_403_ERR = "What Are You Doing? You are Banned"
     ABS_TEXT = "What Are You Trying To Do,Mate?"
     UPGRADE_TEXT = "CONTACT @AID_3N"
-    DOWNLOAD_START = "Trying to Download......"
-    UPLOAD_START = "Trying to Upload......."
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
+    DOWNLOAD_START = "Give Me Some Time Niggah(〒﹏〒)"
+    UPLOAD_START = "Yay, Completed Downloading(;ŏ﹏ŏ)"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It? use @PUBLIC_LEECH_2"
     AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me😒😒**"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds \nUSE @PUBLIC_LEECH_2 FOR UPLOADING FILES FROM LINKS"
     NOT_AUTH_USER_TEXT = "CONTACT @AID_3N"
     NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @AID_3N"
-    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail saved ✅ This Will Be Used For One File Only"
-    DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully."
+    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail Saved ✅ This Is Permanent Thumbnail ᕙ(  • ‿ •  )ᕗ "
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully🤦"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "File Downloaded Successfully😎"
-    CUSTOM_CAPTION_UL_FILE = " "
+    CUSTOM_CAPTION_UL_FILE = "@PUBLIC_LEECH_2"
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found,SED🤒"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     HELP_USER = """Hai {}
