8d4d1ad42912befe0e9790fe76c423df706a498d
strange552 <64140254+strange552@users.noreply.github.com>
2020-06-07 17:06:24 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -6,35 +6,25 @@ SEE /help For My Features"""
 
     RENAME_403_ERR = "There Is Some Error With The File"
     ABS_TEXT = "What Are You Trying To Do,Mate?"
-    UPGRADE_TEXT = "CONTACT @AID_3N"
-    DOWNLOAD_START = "Give Me Some Time Niggah(〒﹏〒)"
+    UPGRADE_TEXT = "CONTACT @adrewjons"
+    DOWNLOAD_START = "Give Me Some Time mate(〒﹏〒)"
     UPLOAD_START = "Yay, Completed Downloading(;ŏ﹏ŏ)"
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It? use @PUBLIC_LEECH_2"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me,For Downloading Files Join @PUBLIC_LEECH_2**"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds Use @PUBLIC_LEECH_2 To Download From Links"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It?"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {}"
     NOT_AUTH_USER_TEXT = "CONTACT @AID_3N"
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @AID_3N"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @adrewjons"
     SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail Saved ✅ This Is Permanent Thumbnail ᕙ(  • ‿ •  )ᕗ "
     DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully🤦"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "File Downloaded Successfully😎"
     CUSTOM_CAPTION_UL_FILE = """✯ ━━━━━━ ✧ ━━━━━━ ✯
-➾ᴄʜᴀɴɴᴇʟ : <a href="https://t.me/CPRLATESTMOVIES">ᴄᴘʀ ᴍᴏᴠɪᴇs</a>
-➾ᴄʜᴀɴɴᴇʟ : <a href="https://t.me/joinchat/AAAAAE5zacv7WFZypKR-vA">ᴄᴘʀ ᴛᴀᴍɪʟ</a>
-➾ᴄʜᴀɴɴᴇʟ : <a href="https://t.me/joinchat/AAAAAEVY-dDxRxV-ue8lwA">ᴄᴘʀ ʜɪɴᴅɪ</a>
-➾ᴄʜᴀɴɴᴇʟ : <a href="https://t.me/joinchat/AAAAAFEfc6lWc3Blve9djQ">ᴄᴘʀ ᴍᴀʟᴀʏᴀʟᴀᴍ</a> 
-➾ᴄʜᴀɴɴᴇʟ : <a href="https://t.me/CPR_HEVC">ᴄᴘʀ ʜᴇᴠᴄ</a>
-➾ᴄʜᴀɴɴᴇʟ : <a href="https://t.me/joinchat/AAAAAE2BTH_3HSMkRhpXew">ᴄᴘʀ ᴛᴠ sᴇʀɪᴇs</a>
-➾ᴄʜᴀɴɴᴇʟ : <a href="https://t.me/joinchat/AAAAAEsDIRVFK1YL_1u-tQ">ᴍᴏʙɪʟᴇ ᴍᴏᴠɪᴇs</a>
-➾ᴄʜᴀɴɴᴇʟ : <a href="https://t.me/joinchat/AAAAAES6ylIS7Zs1iw6SHQ">ᴄᴘʀ sᴛʀᴇᴀᴍɪɴɢ</a>
-
-..𝓒𝓲𝓷𝓮𝓶𝓪 𝓟𝓻𝓪𝓷𝓽𝓱𝓪𝓷𝓶𝓪𝓻."""
     HELP_USER = """How To Use Me
+    
     1. Send me any Telegram File(Some Files Gets Error)
     2. Reply to that message to /rename NEW NAME.EXTENSION
     FEATURES
     ➡Rename Telegram File With Custom Thumbnail.
     ➡Convert Streamable File(Video) To File.
-    ➡ FOR Downloading Files At Highspeed Join @PUBLIC_LEECH_2"""
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply The New Name With /rename Filename.extn"
     IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
