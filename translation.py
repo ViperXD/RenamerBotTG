3eacb1fc582cb005b041cb2773f0dbde435dc5e0
Jason-Brody <65779952+Jason-Brody@users.noreply.github.com>
2020-06-19 21:48:08 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -7,18 +7,30 @@ SEE /help For My Features"""
     RENAME_403_ERR = "There Is Some Error With The File"
     ABS_TEXT = "What Are You Trying To Do,Mate?"
     UPGRADE_TEXT = "CONTACT @AID_3N"
-    DOWNLOAD_START = "Give Me Some Time Niggah(〒﹏〒)"
-    UPLOAD_START = "Yay, Completed Downloading(;ŏ﹏ŏ)"
+    DOWNLOAD_START = "Downloading The File"
+    UPLOAD_START = "Uploading........."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It? use @PUBLIC_LEECH_2"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me,For Downloading Files Join @PUBLIC_LEECH_2**"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds Use @PUBLIC_LEECH_2 To Download From Links"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Send Next File Please**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
     NOT_AUTH_USER_TEXT = "CONTACT @AID_3N"
     NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @AID_3N"
-    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail Saved ✅ This Is Permanent Thumbnail ᕙ(  • ‿ •  )ᕗ "
+    SAVED_CUSTOM_THUMB_NAIL = "Thumbnail Set Aaayii"
     DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully🤦"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "File Downloaded Successfully😎"
-    CUSTOM_CAPTION_UL_FILE = """@CPRLINKS"""
+    CUSTOM_CAPTION_UL_FILE = """╔══════════ ∘◦ ⛧ﾐ ◦∘  ═══════════╗
+       𝘊𝘏𝘈𝘕𝘕𝘌𝘓➡️ @CPR_Malayalam     
+       𝘊𝘏𝘈𝘕𝘕𝘌𝘓➡️ @CPR_Tamil                 
+       𝘊𝘏𝘈𝘕𝘕𝘌𝘓➡️ @CPR_HINDI               
+       𝘊𝘏𝘈𝘕𝘕𝘌𝘓➡️ @CPR_HEVC
+       𝘊𝘏𝘈𝘕𝘕𝘌𝘓➡️ @CPR_STREAMING
+       𝘊𝘏𝘈𝘕𝘕𝘌𝘓➡️ @NETFLIX_TVSERIES
+       𝘊𝘏𝘈𝘕𝘕𝘌𝘓➡️ @CPR_DVDINFO
+       𝘊𝘏𝘈𝘕𝘕𝘌𝘓➡️ @CPRMOBILEMOVIES
+       𝘎𝘙𝘖𝘜𝘗    ➡️ @FOREIGNMOVIESLOVERS   
+       𝘎𝘙𝘖𝘜𝘗    ➡️ @CINEMA_PRANTHANMAR    
+╚═══════════ ∘◦ ミ⛧ ◦∘  ═══════════╝
+քʀօʋɨɖɛɖ ɮʏ @CPRLINKS աɨȶɦ ❤️"""
     HELP_USER = """<b>How To Use Me</b>
     1. Send me any Telegram File(Some Files Gets Error)
     2. Reply to that message to /rename NEW NAME.EXTENSION
@@ -27,7 +39,7 @@ SEE /help For My Features"""
     ➡Convert Streamable File(Video) To File.
     ➡ FOR Downloading Files At Highspeed Join @PUBLIC_LEECH_2"""
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply The New Name With /rename Filename.extn"
-    IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
+    IFLONG_FILE_NAME = """Cheriya Name Aaakadeey"""
     REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
     CANCEL_STR = "Process Cancelled"
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
