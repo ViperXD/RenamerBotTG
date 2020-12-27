011410f7ec5838276056ce156a21142ec1409da7
Aid3n_Pearce <61540835+Aid-3n@users.noreply.github.com>
2020-12-27 22:18:50 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 import os
 import sqlite3
-from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
+from pyrogram import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
