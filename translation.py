13144fd5e623e004429b0fe8ca1c5aeecf1e689f
TG Expert <78008481+v-v-r-official@users.noreply.github.com>
2021-03-12 05:57:51 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,16 +3,16 @@ class Translation(object):
 
     RENAME_403_ERR = "There Is Some Error With The File"
     ABS_TEXT = "What Are You Trying To Do,Mate? You Are Banned!"
-    UPGRADE_TEXT = "CONTACT @AID_3N"
+    UPGRADE_TEXT = "CONTACT @VkBotsSupportBot"
     DOWNLOAD_START = "Downloading😊"
     UPLOAD_START = "Uploading✨"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It?"
     AFTER_SUCCESSFUL_UPLOAD_MSG = "**Next File **"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
-    NOT_AUTH_USER_TEXT = "CONTACT @AID_3N"
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @AID_3N"
-    SAVED_CUSTOM_THUMB_NAIL = "Thumbnail Set 😂😂😏"
-    DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail Pooyee🤣🤣"
+    NOT_AUTH_USER_TEXT = "CONTACT @VkBotsSupportBot"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @VkBotsSupportBot"
+    SAVED_CUSTOM_THUMB_NAIL = "Thumbnail Set✅"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail Deleted"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "File Downloaded Successfully😎"
     CUSTOM_CAPTION_UL_FILE = """FILENAME NOT AVAILABLE"""
@@ -22,9 +22,9 @@ class Translation(object):
     FEATURES
     ➡Rename Telegram File With Custom Thumbnail.
     ➡Convert Streamable File(Video) To File.
-    ➡ FOR More Join @DX_BOTZ"""
+    ➡ FOR More Join @VKPROJECTS"""
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply The New Name With /rename Filename.extn"
-    IFLONG_FILE_NAME = """Cheriya Name Aaakadeey"""
+    IFLONG_FILE_NAME = """Make It Small Name"""
     REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
     CANCEL_STR = "Process Cancelled"
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
